da400b0e9a3ee140402b43c02ef39cf165f71d57
RoniIrawan <neffclothingbandung@gmail.com>
2015-02-27 11:28:51 +0700

    Setting in Java PC

--- frontend/views/stok/index.php
+++ frontend/views/stok/index.php
@@ -10,8 +10,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="text-center">
 	<img class="img-rounded" src="<?php echo Yii::getAlias('@web'); ?>/image/basama.jpg" width="350" height="100" alt="BASAMA GROUP">	
-	<?php $form = ActiveForm::begin(); ?>
-	    
+	<?php $form = ActiveForm::begin(); ?>	    
 	    <div class="form-group">
 	        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
 	    </div>
