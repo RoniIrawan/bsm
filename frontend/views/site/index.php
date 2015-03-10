9e9bbd11adcfa1dfbc5eda8708c12a60488b8fbb
roniirawan <neffclothingbandung@gmail.com>
2015-03-10 18:27:20 +0700

    Insert Carousel

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,48 +4,57 @@ $this->title = 'Selamat datang di BSM 67';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+      </ol>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="img-rounded" src="<?php echo Yii::getAlias('@web'); ?>/image/slide/basama.jpg" alt="BASAMA GROUP">
+          <div class="carousel-caption">
+            BASAMA GROUP
+          </div>
         </div>
+        <div class="item">
+          <img class="img-rounded" src="<?php echo Yii::getAlias('@web'); ?>/image/slide/customer.jpg" alt="BASAMA GROUP">
+          <div class="carousel-caption">
+            Customer Satisfaction
+          </div>
+        </div>
+        <div class="item">
+          <img class="img-rounded" src="<?php echo Yii::getAlias('@web'); ?>/image/slide/fashion_show.jpg" alt="BASAMA GROUP">
+          <div class="carousel-caption">
+            Fashion Show
+          </div>
+        </div>
+        <div class="item">
+          <img class="img-rounded" src="<?php echo Yii::getAlias('@web'); ?>/image/slide/shoes_factory.jpg" alt="BASAMA GROUP">
+          <div class="carousel-caption">
+            Shoes Factory
+          </div>
+        </div>
+      </div>
 
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Kembali</span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Berikutnya</span>
+      </a>
     </div>
+
+
+
+
 </div>
--- frontend/views/stok/index.php
+++ frontend/views/stok/index.php
@@ -11,15 +11,16 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="text-center">
 	<img class="img-rounded" src="<?php echo Yii::getAlias('@web'); ?>/image/basama.jpg" width="350" height="100" alt="BASAMA GROUP">
 	<?php $form = ActiveForm::begin(); ?>	    
-	    <div class="form-group-lg">	    	
-    		<?= $form->field($model, 'kode')->textInput(['tabIndex'=>'1',
+	    <div class="form-group-lg">	
+			<div class="col-md-6 col-md-offset-3">
+				<?= $form->field($model, 'kode')->textInput(['tabIndex'=>'1',
 					'id'=>'kode',
-					'placeholder'=>'Silahkan masukan kode barang yang anda cari',
+					'placeholder'=>'masukan kode barang',
 					'maxlength'=>'7',						
 					'style'=>'text-align: center; text-transform: uppercase'
-				])->label('') 
-			?>
-			<?= Html::submitButton('Cek Data Stok', ['class' => 'btn btn-primary']) ?>	    	
+				])->label('') ?>
+				<?= Html::submitButton('Cek Data Stok', ['class' => 'btn btn-primary']) ?>	
+			</div>		    	
 	    </div>	    
 	<?php ActiveForm::end(); ?>
 </div>
