3e34c1f3e6a559d44df7a3948d4dcd4691479eea
RoniIrawan <neffclothingbandung@gmail.com>
2015-03-10 22:31:32 +0700

    Setup Tentang Kami Page

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -2,13 +2,40 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'About';
+$this->title = 'Tentang kami';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+    
+<legend>
+	<p class="lead">Kepada semua mitra <b class="text-info">BASAMA GROUP</b> yang kami hormati,</p>
+</legend>
+<blockquote>
+	<p>Web site ini kami sediakan sebagai media untuk memberikan informasikan ketersediaan/stok produk kami kepada semua pihak yang telah bekerja sama dengan kami.
+    	<b>BSM 67 (BASAMA GROUP)</b> memiliki komitmen untuk selalu meningkatkan pelayanan dan kepuasan Anda.
+    	Semoga informasi yang tersedia di dalam web site ini dapat bermanfaat bagi Anda.</p>
+</blockquote>    
+<p class="text-right lead">Terima kasih.</p><br>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
-
-    <code><?= __FILE__ ?></code>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <a href="#" class="thumbnail">
+      <img class="img-rounded" src="<?php echo Yii::getAlias('@web'); ?>/image/soga2.jpg" width="200" height="75" alt="BSM SOGA">      
+    </a>
+    <h3><p class="text-center"><span class="label label-primary">BSM SOGA</span></p></h3>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <a href="#" class="thumbnail">
+      <img class="img-rounded" src="<?php echo Yii::getAlias('@web'); ?>/image/java2.jpg" width="200" height="75" alt="JAVA SEVEN">
+    </a>
+    <h3><p class="text-center"><span class="label label-default">JAVA SEVEN</span></p></h3>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <a href="#" class="thumbnail">
+      <img class="img-rounded" src="<?php echo Yii::getAlias('@web'); ?>/image/cbr2.jpg" width="200" height="75" alt="CBR SIX">
+    </a>
+    <h3><p class="text-center"><span class="label label-danger">CBR SIX</span></p></h3>
+  </div>
+</div>
+    
 </div>
